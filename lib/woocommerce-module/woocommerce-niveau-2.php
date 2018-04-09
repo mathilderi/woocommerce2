6b35b3dc33746c0721f70f05e8c5c0f3c8404ba2
Unknown <mathirivoire@gmail.com>
2018-04-09 16:43:39 +0200

    Ajout de contenu avant les vignettes

--- lib/woocommerce-module/woocommerce-niveau-2.php
+++ lib/woocommerce-module/woocommerce-niveau-2.php
@@ -1,2 +1,9 @@
 <?php
-wp_die("ça marche");
+//wp_die("ça marche");
+
+// ajout d'un contanu avant les produits  (le chiffre indique un ordre de priorité)
+function woocom2_nouveau_contenu_avant_produit(){
+	echo '<div>Mon texte</div>'; 
+	the_title('<h1>', '</h1>');
+}
+add_action('woocommerce_before_shop_loop_item_title', 'woocom2_nouveau_contenu_avant_produit', 5);
