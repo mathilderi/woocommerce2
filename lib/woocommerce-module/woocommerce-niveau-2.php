4dca64b07c79b29aeaa6e886deef376b9c9fc1f9
Unknown <mathirivoire@gmail.com>
2018-04-10 11:32:17 +0200

    Afficher compositions textile sur fiche produit NOTE
    Pour l'instant contenu statique
    https://businessbloomer.com/woocommerce-visual-hook-guide-single-product-page/

--- lib/woocommerce-module/woocommerce-niveau-2.php
+++ lib/woocommerce-module/woocommerce-niveau-2.php
@@ -14,3 +14,13 @@ add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loo
 // Changement de place pour "prix"; il faut changer la priorité car si on laisse 10 il affiche d'abord l'image qui est programée par woocommerce
 remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
 add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_price', 3 );
+
+
+// fiche produit, nouveau contenu  : ajout d'un contenu après le produit  (le chiffre indique un ordre de priorité)
+
+
+function woocom2_composition_textile(){
+	echo 'mon texte'; 
+	
+}
+add_action('woocommerce_product_meta_end', 'woocom2_composition_textile', 5);
