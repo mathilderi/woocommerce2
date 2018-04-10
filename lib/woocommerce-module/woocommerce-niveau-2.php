5584720344736908c8f6cac252b7cc5559d5d1af
Unknown <mathirivoire@gmail.com>
2018-04-10 15:11:40 +0200

    Masquer un contenu avec ACF, par fiche produit

--- lib/woocommerce-module/woocommerce-niveau-2.php
+++ lib/woocommerce-module/woocommerce-niveau-2.php
@@ -9,8 +9,10 @@ function woocom2_nouveau_contenu_avant_produit(){
 add_action('woocommerce_before_shop_loop_item_title', 'woocom2_nouveau_contenu_avant_produit', 5);
 
 // Changement de place pour "ajouter au panier"
-remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
-add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_add_to_cart', 4);
+//remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+//add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_add_to_cart', 4);
+// enlevé car enlève le lien
+
 // Changement de place pour "prix"; il faut changer la priorité car si on laisse 10 il affiche d'abord l'image qui est programée par woocommerce
 remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
 add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_price', 3 );
@@ -29,8 +31,21 @@ function woocom2_composition_textile(){
 		echo "<p class='compo-textile'>Aucune composition disponible</p>"; 
 	}
 	// affiche en front des infos qu'on a pas là
-	var_dump($compo_textile); exit;
+	//var_dump($compo_textile); exit;
 
-	print_r($compo_textile); exit;
+	//print_r($compo_textile); exit;
 }
 add_action('woocommerce_product_meta_end', 'woocom2_composition_textile', 5);
+
+
+//Retirer les notes sur le produit
+//remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
+
+add_action ('wp_head', 'woocom2_option_titre');
+function woocom2_option_titre(){
+if(get_field('woocom2_masquer_les_avis')){
+
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
+
+	}
+}
